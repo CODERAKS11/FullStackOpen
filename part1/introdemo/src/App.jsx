bfee9869488208ac0dd5da526abf6945382be250
CODERAKS11 <kk9546x@gmail.com>
2025-01-22 01:05:21 +0530

    ex1.12

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -11,29 +11,28 @@ import Button from './components/Button.jsx'
 
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  function getRandomIntInclusive(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  
 
   return (
-    <>
-      
-
     <div>
-      <h1 className="color-red">give feedback</h1>
-      <br /><br />
-      
-      <div className="button-container">
-        <Button text="good" handleClick={() => setGood(good + 1)} />
-        <Button text="neutral" handleClick={() => setNeutral(neutral + 1)} />
-        <Button text="bad" handleClick={() => setBad(bad + 1)} />
-      </div>
-      <br /><br />
-    </div>
-    <Statistics good={good} neutral={neutral} bad={bad} />
-    
-  </> 
+    {anecdotes[selected]} <br />
+    <button onClick={() => setSelected(getRandomIntInclusive(0,anecdotes.length))}>next anecdote</button><br />
+  </div>
   )
 }
 
