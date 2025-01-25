aab3e6c93d5edf6dd5a4c97f79789e0b90dc19b6
CODERAKS11 <kk9546x@gmail.com>
2025-01-25 18:32:40 +0530

    ex2.6

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -1,66 +1,36 @@
-import Course from "./components/Course.jsx"
+import { useState } from 'react'
 
 const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
-    }
-  ]
-console.log(courses)
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addPersons = (event) => {
+    event.preventDefault()
+    setPersons(persons.concat({name: newName}))
+    setNewName('')
+  }
+
   return (
-    <>
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={(e)=>{
+            e.preventDefault(),
+            setNewName(e.target.value)
+            }} />
+        </div>
+        <div>
+          <button type="submit" onClick={addPersons}>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
       {
-        courses.map((course)=> {
-        console.log(course)
-        return<div key={course.id}>
-          <Course course={course}/>
-        </div> 
-          
-        })
+        persons.map((person, index) => <p key={index}>{person.name}</p>)
       }
-    </>
-      
-    
-    
+    </div>
   )
 }
 
