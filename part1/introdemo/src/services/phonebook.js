368c2997c181f6efba8b3018ccb01ba224caae76
CODERAKS11 <kk9546x@gmail.com>
2025-01-27 23:56:23 +0530

    ex2.14 completed

--- part1/introdemo/src/services/phonebook.js
+++ part1/introdemo/src/services/phonebook.js
@@ -15,21 +15,14 @@ const update = (id, newObject) => {
   const request = axios.put(`${baseUrl}/${id}`, newObject)
   return request.then(response => response.data)
 }
-<<<<<<< HEAD
 const deletePerson = (id) => {
     const request = axios.delete(`${baseUrl}/${id}`)
     return request.then(response => response.data)
 }    
-=======
->>>>>>> 2c806fdbbb3ae211361426539636c92064fea338
 
 export default { 
   getAll: getAll, 
   create: create, 
-<<<<<<< HEAD
   update: update,
   deletePerson: deletePerson
-=======
-  update: update 
->>>>>>> 2c806fdbbb3ae211361426539636c92064fea338
 }
