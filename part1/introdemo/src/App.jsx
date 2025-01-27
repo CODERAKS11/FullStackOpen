e3beb0ff7b4df17dabff02b54f05c5c3b3ddba2c
CODERAKS11 <kk9546x@gmail.com>
2025-01-28 02:00:08 +0530

    ex2.17

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -5,6 +5,18 @@ import Persons from './components/Persons'
 import axios from 'axios'
 import phonebook from './services/phonebook'
 
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div className='error'>
+      {message}
+    </div>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
@@ -13,6 +25,7 @@ const App = () => {
     set: false,
     content: ''
   })
+  const [errorMessage, setErrorMessage] = useState('Notification Panel')
   useEffect(() => {
     // const response = axios.get('http://localhost:3001/persons')
     phonebook
@@ -36,11 +49,30 @@ const App = () => {
             setPersons(persons.map(person => 
               person.id !== existingPerson.id ? person : { ...response }
             ))
+            setErrorMessage(
+              `Successfully updated ${existingPerson.name} `
+            )
+            setTimeout(() => {
+              setErrorMessage(null)
+            }, 5000)
           })
           .catch(error => {
-            console.error('Error updating person:', error)
-            alert(`Failed to update ${newName}'s number`)
-          })
+            console.log('Error updating person:', error)
+            if( error.response.data === "Not Found"){
+              setErrorMessage(`Information of ${existingPerson.name} has already been removed from server`)
+              setTimeout(() => {
+                setErrorMessage(null)
+              }, 5000)
+            }else{
+              setErrorMessage(
+                `Failed to update ${existingPerson.name} `
+              )
+              setTimeout(() => {
+                setErrorMessage(null)
+              }, 5000)
+            }})
+            
+            
       }
     } else {
       phonebook
@@ -49,10 +81,23 @@ const App = () => {
           setPersons(persons.concat(response))
           setNewName('')
           setNewNumber('')
+          setErrorMessage(
+            `Successfully added  ${response.name} to the phonebook`
+          )
+          setTimeout(() => {
+            setErrorMessage(null)
+          }, 5000)
+          
         })
         .catch(error => {
           console.error('Error creating person:', error)
-          alert(`Failed to add ${newName}`)
+          
+          setErrorMessage(
+            `Failed to add ${newName}`
+          )
+          setTimeout(() => {
+            setErrorMessage(null)
+          }, 5000)
         })
     }
   }
@@ -66,6 +111,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={errorMessage} />
       <Filter filterValue={filterValue} setFilterValue={setFilterValue} />
       {/* <div>
        filter shown with <input type='text' value={filterValue.content} onChange={(e)=>{
