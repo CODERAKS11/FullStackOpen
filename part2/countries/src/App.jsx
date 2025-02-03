01ede590026afca2955814c350b3df0b0447770f
CODERAKS11 <kk9546x@gmail.com>
2025-02-03 14:05:34 +0530

    ex2.20

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
 import country from './services/country.js';
 import './App.css';
-<<<<<<< HEAD
 import View from './components/View.jsx';
 import axios from 'axios';
 import dotenv from "dotenv"
@@ -120,61 +119,6 @@ console.log(selectedCountryWeatherData)
       }
       
       </>
-=======
-
-function App() {
-  const [countries, setCountries] = useState([]);
-  const [search, setSearch] = useState('')
-  const [filterResults, setFilterResults] = useState([])
-  useEffect(() => {
-    country
-      .getAll()
-      .then((response) => {
-        
-        setCountries(response); // Set the countries state with the response data
-      })
-      .catch((error) => {
-        console.error("Error fetching countries:", error); // Handle errors
-      });
-  }, []); // Empty dependency array ensures this effect runs only once
-
-
-  setFilterResults(countries.filter((country) => country.name.common.toLowerCase().includes(search.content.toLowerCase())))
-     
-  return (
-    <>
-      <div>
-        <p>find countries</p>
-        <input value={search} onChange={setSearch} />
-        
-        {search.length > 0 ? (
-          filterResults.map((filterResult, index) => (
-            <li key={index}>
-              {/* Render country names, assuming 'common' is the name */}
-              {filterResult?.name?.common || 'No name available'}
-            </li>
-          ))
-        ) : (
-          <p>Loading countries...</p>
-        )}
-        
-      </div>
-      <div>
-      <ul>
-        {countries.length > 0 ? (
-          countries.map((country, index) => (
-            <li key={index}>
-              {/* Render country names, assuming 'common' is the name */}
-              {country?.name?.common || 'No name available'}
-            </li>
-          ))
-        ) : (
-          <p>Loading countries...</p>
-        )}
-      </ul>
-    </div>
-    </>
->>>>>>> cb79b4f9d2aa0b0e591dc56561e8f87b43770e72
     
   );
 }
